3682e1f1ade0b6f33c4447cc51abffa3fee9c7a8
stewartmmt <61957332+stewartmmt@users.noreply.github.com>
2023-09-20 20:35:28 +0800

    1stpush

--- src/main.c
+++ src/main.c
@@ -3,10 +3,9 @@
 #include"Delay.h"
 #include"MatrixKey.h"
 void main(){
-
-
+    unsigned int password;
     LCD_Init();
-    LCD_ShowString(1,1,"Password:");
+    LCD_ShowString(1,1,"What'sTheAnswer");
     LCD_ShowBinNum(2,1,0x0,4);
 
     while (1)
@@ -14,10 +13,31 @@ void main(){
 
         if(KeyNum)
             {
-                LCD_ShowNum(2,1,KeyNum,2);
-                Delay(2);
+            if(KeyNum<11)
+                {
+                password *= 10;
+                password += KeyNum % 10 ; 
+
+                }              
+            //clear
+            if (KeyNum == 12)               
+               {password = 0;
+               LCD_ShowString(2,6,"\0\0\0\0\0\0\0\0\0\0\0");}
+            //delete
+            if (KeyNum == 11)
+                password /= 10;
+            //submit
+            if(KeyNum == 13){
+                if(password == 42){
+                    LCD_ShowString(2,6,"Correct");
+                }
+                else
+                    LCD_ShowString(2,6,"WRONG");
             }
-        
+            }
+            
+            LCD_ShowNum(2,1,password,4);
+            Delay(1);            
     }
     
 }
